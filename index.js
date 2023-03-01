5a87bae461cd84713933780e1378725a4286ca9e
Loirise <wieczorekszymon4@gmail.com>
2023-03-01 18:42:18 +0100

    added express-router, flash messages, basic cookies and session implementation and did some clean up

--- index.js
+++ index.js
@@ -3,131 +3,63 @@ const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const TrainingPlan = require('./models/trainingPlan');
-const wrapAsync = require('./utils/wrapAsync');
+const session = require('express-session');
+const flash = require('connect-flash'); 
+
 const ExpressError = require('./utils/ExpressError');
-const { trainingplanSchema } = require('./joiSchemas.js');
+const trainingplans = require('./routes/trainingplans');
 
 const app = express();
 const db = mongoose.connection;
 
-exercisesList = [
-    'bar dip',
-    'bench press',
-    'dumbbell chest press',
-    'push up',
-    'barbell front raise',
-    'cable lateral raise',
-    'dumbbell rear delt row',
-    'face pull',
-    'barbell curl',
-    'dumbbell curl',
-    'cable curl',
-    'hammer curl',
-    'bench dip',
-    'close-grip push up',
-    'barell lying triceps extension',
-    'overhead cable triceps extension',
-    'barbell lunge',
-    'front squat',
-    'goblet squat',
-    'pause squat',
-    'bulgarian split squat',
-    'barbell squat',
-    'side squat',
-    'back extension',
-    'barbell row',
-    'chin-up',
-    'pull-up',
-    'deadlift',
-    'kettlebell swing',
-    'barbell shrug',
-    'lat pulldown',
-    'romanian deadlift',
-    'hip thrust',
-    'cable crunch',
-    'hanging leg raise',
-    'oblique crunch',
-    'plank',
-    'side plank',
-    'sit-up',
-    'mountain climbers',
-    'heel raise',
-    'standing calf raise',
-    'seater calf raise',
-    'wrist curl',
-    'farmer walk',
-    'wrist extension'
-]
 
+/* connect to mongodb */
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/fitworkouts')
-
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => {
     console.log('Database connected');
 });
 
+
 app.engine('ejs', ejsMate);
 app.set('view enginge', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-/* app.use('/public', express.static('public')); */
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const validatePlan = (req, res, next) => {
-    const { error } = trainingplanSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(400, msg)
-    } else {
-        next();
-    };
+/* session */
+const sessionConfig = {
+    secret: 'supersecretstuff123!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
 };
+app.use(session(sessionConfig));
+/* flash */
+app.use(flash());
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+/* express router */
+app.use('/trainingplans', trainingplans);
+
+
+/* routes */
 app.get('/', (req, res) => {
     res.render('home.ejs');
 });
-
-app.get('/trainingplans', wrapAsync(async (req, res) => {
-    const trainingplans =  await TrainingPlan.find({});
-    res.render('trainingplans/index.ejs', {trainingplans})
-}));
-
-app.get('/trainingplans/new', (req, res) => {
-    res.render('trainingplans/new.ejs', {exercisesList});
-});
-
-app.post('/trainingplans', validatePlan, wrapAsync(async (req, res) => {
-    /* save to db */
-    const newPlan = new TrainingPlan(req.body.trainingplan);
-    await newPlan.save();
-    res.redirect(`/trainingplans/${newPlan._id}`)
-}));
-
-app.get('/trainingplans/:id', wrapAsync(async (req, res) => {
-    const plan = await TrainingPlan.findById(req.params.id)
-    res.render('trainingplans/show.ejs', {plan});
-}));
-
-app.get('/trainingplans/:id/edit', wrapAsync(async (req, res) => {
-    const plan = await TrainingPlan.findById(req.params.id)
-    res.render('trainingplans/edit.ejs', {plan, exercisesList});
-}));
-
-app.put('/trainingplans/:id', validatePlan, wrapAsync(async (req,res) => {
-    /* get the id of the plan */
-    const { id } = req.params;
-    /* find in db by id and update */
-    const editedPlan = await TrainingPlan.findByIdAndUpdate(id, { ...req.body.trainingplan });
-    res.redirect(`/trainingplans/${editedPlan._id}`)
-}));
-
-app.delete('/trainingplans/:id', wrapAsync(async (req, res) => {
-    const {id} = req.params;
-    await TrainingPlan.findByIdAndDelete(id);
-    res.redirect('/trainingplans');
-}));
 
 app.all('*', (req, res, next) => {
     next(new ExpressError(404, 'Page Not Found'));
---
+++ routes/trainingplans.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const router = express.Router();
+
+const ExpressError = require('../utils/ExpressError');
+const wrapAsync = require('../utils/wrapAsync');
+const TrainingPlan = require('../models/trainingPlan');
+const { trainingplanSchema } = require('../joiSchemas');
+
+const validatePlan = (req, res, next) => {
+    const { error } = trainingplanSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(400, msg)
+    } else {
+        next();
+    };
+};
+
+exercisesList = [
+    'bar dip',
+    'bench press',
+    'dumbbell chest press',
+    'push up',
+    'barbell front raise',
+    'cable lateral raise',
+    'dumbbell rear delt row',
+    'face pull',
+    'barbell curl',
+    'dumbbell curl',
+    'cable curl',
+    'hammer curl',
+    'bench dip',
+    'close-grip push up',
+    'barell lying triceps extension',
+    'overhead cable triceps extension',
+    'barbell lunge',
+    'front squat',
+    'goblet squat',
+    'pause squat',
+    'bulgarian split squat',
+    'barbell squat',
+    'side squat',
+    'back extension',
+    'barbell row',
+    'chin-up',
+    'pull-up',
+    'deadlift',
+    'kettlebell swing',
+    'barbell shrug',
+    'lat pulldown',
+    'romanian deadlift',
+    'hip thrust',
+    'cable crunch',
+    'hanging leg raise',
+    'oblique crunch',
+    'plank',
+    'side plank',
+    'sit-up',
+    'mountain climbers',
+    'heel raise',
+    'standing calf raise',
+    'seater calf raise',
+    'wrist curl',
+    'farmer walk',
+    'wrist extension'
+]
+
+
+router.get('/', wrapAsync(async (req, res) => {
+    const trainingplans =  await TrainingPlan.find({});
+    res.render('trainingplans/index.ejs', {trainingplans})
+}));
+
+router.get('/new', (req, res) => {
+    res.render('trainingplans/new.ejs', {exercisesList});
+});
+
+router.post('/', validatePlan, wrapAsync(async (req, res) => {
+    const newPlan = new TrainingPlan(req.body.trainingplan);
+    await newPlan.save();
+    req.flash('success', 'Successfully made new plan!');
+    res.redirect(`/trainingplans/${newPlan._id}`);
+}));
+
+router.get('/:id', wrapAsync(async (req, res) => {
+    const plan = await TrainingPlan.findById(req.params.id);
+    console.log(plan)
+    if(!plan){
+        req.flash('error', 'Cannot find that workout plan');
+        return res.redirect('/trainingplans');
+    }
+    res.render('trainingplans/show.ejs', {plan});
+}));
+
+router.get('/:id/edit', wrapAsync(async (req, res) => {
+    const plan = await TrainingPlan.findById(req.params.id);
+    if(!plan){
+        req.flash('error', 'Cannot find that workout plan');
+        return res.redirect('/trainingplans');
+    }
+    res.render('trainingplans/edit.ejs', {plan, exercisesList});
+}));
+
+router.put('/:id', validatePlan, wrapAsync(async (req,res) => {
+    const { id } = req.params;
+    const editedPlan = await TrainingPlan.findByIdAndUpdate(id, { ...req.body.trainingplan });
+    req.flash('success', 'Successfully edited the plan.');
+    res.redirect(`/trainingplans/${editedPlan._id}`);
+}));
+
+router.delete('/:id', wrapAsync(async (req, res) => {
+    const {id} = req.params;
+    await TrainingPlan.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted the plan.');
+    res.redirect('/trainingplans');
+}));
+
+module.exports = router;
